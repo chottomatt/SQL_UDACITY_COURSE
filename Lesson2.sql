c34127de60654e30223f8757f33306c68842f66b
chottomatt <matheusrocha0110@gmail.com>
2026-08-18 14:14:09 -0300

    .

--- Lesson2.sql
+++ Lesson2.sql
@@ -14,8 +14,8 @@ SELECT *
 
 /* PRACTICE 1
 orders in the orders table. Include the id, 
-occurred_at, and total_amt_usd.
-SELECT id, occurred_at, total_amt_usd */
+occurred_at, and total_amt_usd.*/
+SELECT id, occurred_at, total_amt_usd 
 FROM orders
 ORDER BY occurred_at
 LIMIT 10
