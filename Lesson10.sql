eba8ecfa51367ed47eb70071c854cf9490dfad1f
chottomatt <matheusrocha0110@gmail.com>
2026-08-19 18:54:41 -0300

    Changes made to Lesson 10 and 11

--- Lesson10.sql
+++ Lesson10.sql
@@ -21,14 +21,12 @@ where the standard_qty is zero and either the
 gloss_qty or poster_qty is over 1000.*/
 SELECT *
 FROM orders
-WHERE standard_qty = 0 AND gloss_qty < 1000 OR poster_qty < 1000
+WHERE standard_qty = 0 AND (gloss_qty < 1000 OR poster_qty < 1000);
 /* Find all the company names that start with 
 a 'C' or 'W', and the primary contact contains 
 'ana' or 'Ana', but it doesn't contain 'eana'.*/
-SELECT name
+SELECT *
 FROM accounts
-WHERE name LIKE 'C' 
-or name LIKE 'W' 
-AND name LIKE '%ana%' 
-OR name LIKE '%Ana%' 
-AND name NOT LIKE'%eana%';
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+              AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+              AND primary_poc NOT LIKE '%eana%');
